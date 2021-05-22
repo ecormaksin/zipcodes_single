0af1af37fb38193aa6e457d050f844b699d9ff7f
ecormaksin <truth_jp_4133@yahoo.co.jp>
2021-05-22 22:52:26 +0900

    vaultの使用をやめ環境変数を使用するよう変更

--- server_service/start_app.sh
+++ server_service/start_app.sh
@@ -2,9 +2,24 @@
 
 cd `dirname $0`
 
-VAULT_PASSWORD_FILE_NAME=vault_passwd
-export VAULT_TOKEN=`cat "./vault/${VAULT_PASSWORD_FILE_NAME}"`
+if [ -n "${SPRING_DATASOURCE_URL}"]; then
+    echo "環境変数: SPRING_DATASOURCE_URL を指定してください。"
+    exit 1
+fi
 
-sh ./vault/start_vault.sh 0 "${VAULT_PASSWORD_FILE_NAME}"
+if [ -n "${SPRING_DATASOURCE_USERNAME}"]; then
+    echo "環境変数: SPRING_DATASOURCE_USERNAME を指定してください。"
+    exit 1
+fi
 
-java -jar ./zipcodes.jar --spring.profiles.active=production
+if [ -n "${SPRING_DATASOURCE_PASSWORD}"]; then
+    echo "環境変数: SPRING_DATASOURCE_PASSWORD を指定してください。"
+    exit 1
+fi
+
+if [ -n "${LOGGING_FILE_NAME}"]; then
+    echo "環境変数: LOGGING_FILE_NAME を指定してください。"
+    exit 1
+fi
+
+java -jar ./zipcodes.jar
